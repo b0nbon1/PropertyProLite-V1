03723201ae3657320d75d20927283d9528472c79
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-06-26 16:13:46 +0300

    [feature #166857759] initial user

---
+++ database/Users.js
@@ -0,0 +1,14 @@
+const Users = [
+    {
+        id: 1,
+        firstname: 'bonbon',
+        lastname: 'vic',
+        email: 'bonbov@test.com',
+        phoneNumber: '+254742087558',
+        address: ' kenya',
+        password: '$2b$10$YCjLovXVlqLsfkcYMGNkHeJT3u4edrwcKMHnZ0GjEgMc0WvC7fhqe',
+        isAdmin: true,
+    },
+];
+
+export default Users;
