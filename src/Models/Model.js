03514bf2aad803de520cbeffb378b95f67cbaec5
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-06-26 16:16:25 +0300

    [feature #166857759]create super class models

---
+++ src/Models/Model.js
@@ -0,0 +1,11 @@
+export default class Model {
+    constructor(payload = null) {
+        this.payload = payload;
+        this.result = null;
+    }
+
+    async save(db, obj) {
+        db.push(obj);
+        this.result = obj;
+    }
+}
