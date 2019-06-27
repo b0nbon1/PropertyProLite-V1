e63d450426b927834239f5f51d88143a3d8b6057
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-06-27 06:25:58 +0300

    [feature #166857767]add route login user

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,7 @@ import Validation from '../utils/middleware/validators';
 
 const route = express.Router();
 
-route.post('/register', Validation.validateRegisterUser, Authentication.register);
+route.post('/register', Validation.validateUser, Authentication.register);
+route.post('/login', Validation.validateLogin, Authentication.loginUser);
 
 export default route;
