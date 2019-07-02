adb46979d677905974cbf4562313d50a187c97d1
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-02 14:24:02 +0300

    [starts #166858590] adds failing tests

--- src/test/propertyTest.js
+++ src/test/propertyTest.js
@@ -1,8 +1,10 @@
+/* eslint-disable one-var-declaration-per-line */
+/* eslint-disable one-var */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../app';
 
-let token;
+let token, wrongUser;
 
 chai.use(chaiHttp);
 chai.should();
@@ -17,6 +19,24 @@ describe('Property', () => {
             .end((err, res) => {
                 // eslint-disable-next-line prefer-destructuring
                 token = res.body.token;
+                if (err) return done();
+                done();
+            });
+    });
+    before('generate token for wrong user', (done) => {
+        chai.request(app)
+            .post('/api/v1/register')
+            .send({
+                firstname: 'bonbon',
+                lastname: 'vic',
+                email: 'bonbo@tasty.com',
+                phoneNumber: '+254742087558',
+                address: 'kenya',
+                password: 'f5e4xhr43dh4t',
+            })
+            .end((err, res) => {
+                // eslint-disable-next-line prefer-destructuring
+                wrongUser = res.body.token;
                 if (err) return done();
                 done();
             });
@@ -251,6 +271,156 @@ describe('Property', () => {
                     type: 'one bedroom',
                     address: '5th&%$#',
                     imageUrl: 'https://user-images.githubusercontent.com/46062609/60184047-08807800-9830-11e9-913c-cb55d650f858.PNG',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please enter a valid address of property');
+                    if (err) return done();
+                    done();
+                });
+        });
+    });
+    describe('Update advert', () => {
+        it('should update advert successfully', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    price: 600,
+                    state: 'Rwanda',
+                    city: 'Kigali',
+                    type: 'two bedroom',
+                    address: '5th street',
+                    imageUrl: 'https://user-images.githubusercontent.com/46062609/60184047-08807800-9830-11e9-913c-cb55d650f858.PNG',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please enter a valid address of property');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should owner of the post should update his/her own posts', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    price: 600,
+                    state: 'Rwanda',
+                    city: 'Kigali',
+                    type: 'two bedroom',
+                    address: '5th street',
+                    imageUrl: 'https://user-images.githubusercontent.com/46062609/60184047-08807800-9830-11e9-913c-cb55d650f858.PNG',
+                })
+                .set('authorization', `Bearer ${wrongUser}`)
+                .end((err, res) => {
+                    res.should.have.status(406);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('None of the ads with such id belongs to you');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should update existing post', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/20')
+                .send({
+                    price: 600,
+                    state: 'Rwanda',
+                    city: 'Kigali',
+                    type: 'two bedroom',
+                    address: '5th street',
+                    imageUrl: 'https://user-images.githubusercontent.com/46062609/60184047-08807800-9830-11e9-913c-cb55d650f858.PNG',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('This advert post does not exist');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should not update with no data', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({})
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please update ad with some data');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should property should have Valid price', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    price: 't#',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Price should be a number');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should property should have a valid State', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    state: 'K@',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please enter valid State');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should property should have a valid city name', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    city: 'N@$',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please enter valid city');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should property should have a valid type that exists', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    type: 'one%#%6',
+                })
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.message.should.equal('Please enter a valid type of property');
+                    if (err) return done();
+                    done();
+                });
+        });
+        it('should property should have a valid address', (done) => {
+            chai.request(app)
+                .post('/api/v1/property/1')
+                .send({
+                    address: '5th&%$#',
                 })
                 .set('authorization', `Bearer ${token}`)
                 .end((err, res) => {
