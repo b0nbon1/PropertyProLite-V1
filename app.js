379565bcf6a86cec3b49a675111799c67c5fe1bc
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-05 09:48:45 +0300

    [bug #167101004] fixes on route

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 const prefix = '/api/v1';
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to our restful API');
+    res.status(200).send('Welcome to property pro lite');
 });
 app.use(prefix, users);
 app.use(prefix, property);
