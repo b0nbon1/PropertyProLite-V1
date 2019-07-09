e312d36f354be2379ffc5cb73a6f312c82d21e74
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-09 23:15:33 +0300

    chore (fileupload): fileupload middleware[chore #167128848]

--- src/routes/PropertyRoutes.js
+++ src/routes/PropertyRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import fileupload from 'express-fileupload';
 import Property from '../controllers/PropertyController';
 import Validations from '../utils/middleware/validators';
 import Token from '../utils/middleware/Auth';
 
 const route = express.Router();
 
+route.use(fileupload({ useTempFiles: true }));
 route.post('/property', Validations.property, Token.userToken, Property.Post);
 route.patch('/property/:property_id', Token.userToken,
     Validations.update, Property.Update);
