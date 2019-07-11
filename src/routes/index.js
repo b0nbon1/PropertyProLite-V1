0483f002049c3c37a64c309e67c4ee72c529a881
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-11 16:19:39 +0300

    chore(routes): handles error for wrong routes[finishes #167222082]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import user from './UserRoutes';
 import property from './PropertyRoutes';
+import Res from '../utils/helpers/responses';
 
 const prefix = '/api/v1';
 
 const router = (app) => {
     app.use(prefix, user);
     app.use(prefix, property);
+    // Res.handleError below acts as success
+    app.get('/', (req, res) => Res.handleError(200, 'Welcome to Property Pro Lite', res));
+    app.use('*', (req, res) => Res.handleError(405, 'Invalid route or Method', res));
 };
 
 export default router;
