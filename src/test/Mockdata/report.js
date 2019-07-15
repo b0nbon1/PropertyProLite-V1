fe8114c571d2542bfa5d9da69a63133f1f6326d4
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-15 18:45:31 +0300

    chore(data): adds mockdata [finishes #167271813]

---
+++ src/test/Mockdata/report.js
@@ -0,0 +1,24 @@
+const Property = {
+    report1: {
+        reason: 'price',
+        description: 'The price is to high for this property',
+    },
+    report2: {
+        reason: '',
+        description: 'The price is to high for this property',
+    },
+    report3: {
+        reason: 'price',
+        description: '',
+    },
+    report4: {
+        reason: 'price',
+        description: 'The price is to high for this property',
+    },
+    report5: {
+        reason: 'price',
+        description: 'The price is to high for this property',
+    },
+};
+
+export default Property;
